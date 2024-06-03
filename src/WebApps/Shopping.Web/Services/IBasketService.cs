717a49dcc3d047edfe894fdd6a8d4b76df358f59
Thupden Bhutia <thupdenbhutia281199@gmail.com>
2024-06-03 21:07:09 +0530

    presentationLayer

---
+++ src/WebApps/Shopping.Web/Services/IBasketService.cs
@@ -0,0 +1,44 @@
+﻿using Shopping.Web.Models.Basket;
+
+namespace Shopping.Web.Services
+{
+    public interface IBasketService
+    {
+        [Get("/basket-service/basket/{userName}")]
+        Task<GetBasketResopnse> GetBasket(string userName);
+
+        [Post("/basket-service/basket")]
+        Task<StoreBasketResponse> StoreBasket(StoreBasketRequest request);
+
+        [Delete("/basket-service/basket/{userName}")]
+        Task<DeleteBasektResponse> DeleteBasekt(string userName);
+
+        [Post("/basket-service/basket/checkout")]
+        Task<CheckoutBasketResponse> CheckoutBasket(CheckoutBasketRequest request);
+
+        public async Task<ShoppingCartModel> LoadUserBasket()
+        {
+            var userName = "swn";
+            ShoppingCartModel basket;
+
+            try
+            {
+                var getBasketResponse = await GetBasket(userName);
+                basket = getBasketResponse.Cart;
+            }
+            catch (Exception)
+            {
+                basket = new ShoppingCartModel
+                {
+                    UserName = userName,
+                    Items = []
+                };
+            }
+
+            return basket;
+        }
+    }
+
+   
+}
+
